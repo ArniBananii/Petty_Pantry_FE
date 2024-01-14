bdf3a746b72d36083d6819e56ecafc5b2b5b9f6f
Arno Heitmann <a.f.heitmann@gmail.com>
2024-01-14 21:49:03 +0100

    build: changed base url route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+} from "vue-router";
 import LoginView from "@/views/LoginView.vue";
 import PantryView from "@/views/PantryView.vue";
 import RegistrationView from "@/views/RegistrationView.vue";
@@ -6,7 +10,7 @@ import AddIngredientView from "@/views/AddIngredientView.vue";
 import AboutView from "@/views/AboutView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
